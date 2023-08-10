582fdc86e1c9cb193807a21d1f0963c110557db6
Kaique-Santos17 <80688242+Kaique-Santos17@users.noreply.github.com>
2023-08-10 11:20:25 -0300

    Update variaveisOperadores.js

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,12 @@
+var a = 3;
+var b = 4; 
+
+var soma = a+b; 
+var subtração = a-b;
+var multiplicação = a*b;
+var divisão = a/b;
+
+console.log("O resultado da operação de soma entre"+ a +"+"+ b+ "="+ soma);
+console.log("O resultado da operação de subtração entre"- a-"-"-b-"="-subtração);
+console.log("O resultado da operação de multiplicação entre"* a*"*"*b*"="*multiplicação);
+console.log("O resultado da operação de divisão entre"/ a/ "/"/b/"="/divisão);
